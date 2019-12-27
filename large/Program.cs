e128c94206f4a22240022867fdb8b0bb15cbfe6c
Иван Разинкин <razikin96@mail.ru>
2019-12-27 17:10:50 +0300

    Добавьте файлы проекта.

---
+++ large/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace large
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Pis = new family(5)
+            {
+               Surename="Пискарев"
+            };
+            Console.WriteLine(Pis.ToString());
+        }
+    }
+}
---
+++ large/family.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace large
+{
+    class family
+    {
+        public delegate void coc();
+        public event coc c;
+        public int Count_of_child { get; set; }
+        public string Surename { get; set; }
+        public bool large { get; set; }
+        public override string ToString()
+        {
+            return $"Фамилия семьи:{Surename} Многодетная:{large}";
+        }
+        public family(int coc)
+        {
+            if (coc >= 3)
+            {
+                c?.Invoke();
+                large = true;
+            }
+        }
+    }
+}
